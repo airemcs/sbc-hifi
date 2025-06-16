7a178bd07f833d436c0850679b7817af9cf1f934
airemcs <its.airemcs@gmail.com>
2025-06-16 17:26:29 +0800

    Install Montserrat Font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
+  <>
+  
+  <div className="">
+
+    <div className="w-full h-[99.33px] bg-[#2C3B55]" />
+    <div className="w-full h-[300px] bg-[#262626]" />
+
+    <div className="w-full h-[75.92px] bg-[#DBDBDBA3] items-center content-center">
+      <span className="flex justify-center font-montserrat">Stay safe and bank online! Check out our online bank services here.</span>  
+    </div>
+
+
+
+  </div>
+  
+  </>
   )
 }
